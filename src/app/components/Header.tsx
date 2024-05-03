005c7f728398ed6c7d47bc407d835fb309a30015
b-eiji-mitsumizo <eiji.mitsumizo@mail.benesse.co.jp>
2024-05-03 11:24:23 +0900

    GitHubへの動線を作成

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../globals.css";
+import Link from "next/link";
 
 type HeaderProps = {
   name: string;
@@ -9,6 +10,14 @@ const Header = (props: HeaderProps) => {
   return (
     <header className="fixed top-0 w-full bg-lime-300 text-black text-3xl p-4 text-center shadow-md z-50">
       {props.name}
+      <Link
+        href="https://github.com/b-eiji-mitsumizo/stateManageLibCompare"
+        className="absolute right-4 top-2 font-bold py-2 px-4 rounded hover:text-gray-700"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        To GitHub
+      </Link>
     </header>
   );
 };
---
+++ src/app/router/zustand/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/components/Header";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Header name="Zustand" />
+    </>
+  );
+};
+
+export default page;
